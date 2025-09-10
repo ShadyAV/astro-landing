62d0686344a376c81a344238e36f4b681a830032
avshady <jshadymail@gmail.com>
2025-09-10 12:22:08 +0700

    add GitHub Pages deployment workflow and update Astro config for site and base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
     server: {
         port: 3000,
+        site: 'https://ShadyAV.github.io',
+        base: '/astro-landing',
     },
 });
