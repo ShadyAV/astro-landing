1961b8c1dfb489b5ae0a9af29b517e4535e46c00
avshady <jshadymail@gmail.com>
2025-09-10 15:53:29 +0700

    update Astro config and comet icon path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+    site: 'https://ShadyAV.github.io',
+    base: '/astro-landing',
     server: {
         port: 3000,
-        site: 'https://ShadyAV.github.io',
-        base: '/astro-landing',
     },
 });
